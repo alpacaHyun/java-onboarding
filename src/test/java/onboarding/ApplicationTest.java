db8ba438f1e1f11c879aad0b43718a1991d05ed0
alpacaHyun <flaehdan@gmail.com>
2022-12-09 19:50:24 +0900

    feat : 앞 페이지가 클 경우 에러 확인
    body

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -33,6 +33,13 @@ class ApplicationTest {
             int result = -1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
+        @Test
+        void case4() {
+            List<Integer> pobi = List.of(102, 99);
+            List<Integer> crong = List.of(211, 212);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
     }
 
     @Nested
